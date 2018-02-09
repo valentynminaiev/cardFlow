b8912ae60c52597bc306f9f0efb55f5655e52aaf
valentynminaiev <valentin.minaev@gmail.com>
2018-02-09 10:39:20 +0200

    add Icons to header and TabNav

--- src/navigationConfig.js
+++ src/navigationConfig.js
@@ -1,20 +1,62 @@
 import React from "react";
-import { Text, View } from "react-native";
+import styled from "styled-components";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { StackNavigator, TabNavigator } from "react-navigation";
 import TransactionsScreen from "./screens/Transactions";
 import MyCardsScreen from "./screens/MyCards";
 import NewCardScreen from "./screens/NewCard";
 import VerifyCardScreen from "./screens/VerifyCard";
 import CurrenciesList from "./screens/CurrenciesList";
+import TransactionsIcon from "./assets/images/transactions.png";
+import TransactionsIconFocused from "./assets/images/transactions-focused.png";
+import MyCardsIcon from "./assets/images/my-cards.png";
+import MyCardsIconFocused from "./assets/images/my-cards-focused.png";
+import backIcon from "./assets/images/back-icon.png";
+import cardIcon from "./assets/images/card-header-icon.png";
+
+export const BackButton = ({ onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Image source={backIcon} />
+  </TouchableOpacity>
+);
+
+export const CardButton = ({ onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Image source={cardIcon} />
+  </TouchableOpacity>
+);
 
 const TabNav = TabNavigator({
-  Transactions: { screen: TransactionsScreen },
-  MyCards: { screen: MyCardsScreen }
+  Transactions: {
+    screen: TransactionsScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Image source={focused ? TransactionsIconFocused : TransactionsIcon} />
+      )
+    }
+  },
+  MyCards: {
+    screen: MyCardsScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Image source={focused ? MyCardsIconFocused : MyCardsIcon} />
+      )
+    }
+  }
 });
 
-export default StackNavigator({
-  Home: { screen: TabNav },
-  NewCard: { screen: NewCardScreen },
-  CurrenciesList: { screen: CurrenciesList },
-  VerifyCard: { screen: VerifyCardScreen }
-});
+export default StackNavigator(
+  {
+    Home: { screen: TabNav },
+    NewCard: { screen: NewCardScreen },
+    CurrenciesList: { screen: CurrenciesList },
+    VerifyCard: { screen: VerifyCardScreen }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        paddingHorizontal: 10
+      }
+    })
+  }
+);
